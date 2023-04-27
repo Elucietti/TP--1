3864aed2cda92875c16a5b9c9d914ac7828aa62a
Emanuel <emanuellucietti@tdf.edu.ar>
2023-04-27 05:36:34 -0300

    "tp1"

---
+++ contador/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './componentes/header/haeder';
+import Content from './componentes/content/content';
+import Footer from './componentes/footer/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ contador/src/componentes/content/content.jsx
@@ -0,0 +1,9 @@
+import './content.css';
+
+const Content=()=>{
+    return(
+        <div className='content'>content</div>
+    )
+}
+
+export default Content;
